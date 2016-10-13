9f1e63a20a299e7a6a45de4056ab42cf6fea0b52
Sean O'Shea <oshea.ie@gmail.com>
2016-10-13 15:53:44 -0400

    Named Plans with Tracers (#96)
    * Per issue #80 - Adding a named plan unit test which has a tracer.
    
    * Per issue #80 - Adding some more add lifecycle methods asserts for
    named plans.

--- tests/unit/SchedulerTests.swift
+++ tests/unit/SchedulerTests.swift
@@ -93,6 +93,38 @@ class SchedulerTests: XCTestCase {
       }
     }
   }
+  
+  private class ChangeBooleanNamedPlan: NSObject, NamedPlan {
+    var desiredBoolean: Bool
+    
+    init(desiredBoolean: Bool) {
+      self.desiredBoolean = desiredBoolean
+    }
+    
+    func performerClass() -> AnyClass {
+      return Performer.self
+    }
+    
+    public func copy(with zone: NSZone? = nil) -> Any {
+      return ChangeBooleanNamedPlan(desiredBoolean: desiredBoolean)
+    }
+    
+    private class Performer: NSObject, Performing, NamedPlanPerforming {
+      let target: State
+      required init(target: Any) {
+        self.target = target as! State
+      }
+      
+      func addPlan(_ plan: NamedPlan, named name: String) {
+        let testPlan = plan as! ChangeBooleanNamedPlan
+        target.boolean = testPlan.desiredBoolean
+      }
+      
+      func removePlan(named name: String) {
+        
+      }
+    }
+  }
 
   // Verify that two plans of the same type creates only one performer.
   func testTwoSamePlansCreatesOnePerformer() {
@@ -294,6 +326,24 @@ class SchedulerTests: XCTestCase {
     scheduler.removePlan(named: "name_one", from: target)
 
     waitForExpectations(timeout: 0.1)
+  }
+  
+  func testNamedPlansRespectTracers() {
+    let differentPlan = ChangeBooleanNamedPlan(desiredBoolean:true)
+    let state = State()
+    let scheduler = Scheduler()
+    let tracer = StorageTracer()
+    scheduler.addTracer(tracer)
+    
+    scheduler.addPlan(firstViewTargetAlteringPlan, named: "name_one", to: target)
+    scheduler.addPlan(differentPlan, named: "name_two", to: state)
+    
+    XCTAssertEqual(tracer.addedPlans.count, 2)
+    XCTAssert(tracer.addedPlans[0] is NamedPlan)
+    XCTAssert(tracer.addedPlans[1] is ChangeBooleanNamedPlan)
+    
+    XCTAssert(target.text == "removePlanInvokedaddPlanInvoked")
+    XCTAssert(state.boolean)
   }
 
   // A plan that enables hijacking of the delegated performance token blocks.
